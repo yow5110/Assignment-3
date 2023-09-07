6f61411899fe2d8e87871bbc6ef87c7c87fe6b6c
yow5110 <yow5110@psu.edu>
2023-09-07 01:29:19 -0500

    Update coviddata.py

--- Problem-4/coviddata.py
+++ Problem-4/coviddata.py
@@ -1,4 +1,4 @@
-# Week 3 Assignment 4
+# Week 3 Problem 4
 # Make a 2D plot of new COVID19 cases in the UK as a function of day elapsed, using the data below downloaded from WHO. 
 # The final plot should have a horizontal dashed line at 100,000, and two vertical lines marking two peak values of your choice. 
 # The final plot should also include meaningful labels for both axis.
